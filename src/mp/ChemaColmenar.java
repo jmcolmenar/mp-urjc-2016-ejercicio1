7fc8226ab20339d5b5e930157344053eddafd45f
J. M. Colmenar <jmcolmenar@gmail.com>
2016-04-21 15:42:24 +0200

    Contribución inicial Chema

---
+++ src/mp/ChemaColmenar.java
@@ -0,0 +1,14 @@
+package mp;
+
+/**
+ * Clase de prueba en ejercicio.
+ * 
+ * @author J. M. Colmenar
+ */
+public class ChemaColmenar {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola, soy Chema");
+    }
+    
+}
