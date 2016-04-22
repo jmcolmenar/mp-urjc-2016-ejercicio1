d810811baf8d377f966e81a0e78020e97799f111
jjluczyn <kuba.luczyn@gmail.com>
2016-04-22 18:07:28 +0200

    Mi primera clase de git.

---
+++ src/mp/JakubJanLuczyn.java
@@ -0,0 +1,14 @@
+package mp;
+
+/**
+ * Clase de prueba en ejercicio.
+ * 
+ * @author J. M. Colmenar
+ */
+public class JakubJanLuczyn {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola, no soy Chema");
+    }
+    
+}
