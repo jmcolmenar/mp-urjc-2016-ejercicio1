04e0cd88655a57a750d61296cc96df72913a298c
pablogarciabenayas <pablogarciabenayas@gmail.com>
2016-04-22 18:06:04 +0200

    Añadida clase PabloGarcia

---
+++ src/mp/PabloGarcia.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mp;
+
+/**
+ *
+ * @author pablo
+ */
+public class PabloGarcia {
+
+    public static void main(String[] args) {
+        
+        System.out.println("              ,---. \n" +
+"            ,.'-.   \\ \n" +
+"           ( ( ,'\"\"\"\"\"-. \n" +
+"           `,X          `. \n" +
+"           /` `           `._ \n" +
+"          (            ,   ,_\\ \n" +
+"          |          ,---.,'o `. \n" +
+"          |         / o   \\     ) \n" +
+"           \\ ,.    (      .____, \n" +
+"            \\| \\    \\____,'     \\ \n" +
+"          '`'\\  \\        _,____,' \n" +
+"          \\  ,--      ,-'     \\ \n" +
+"            ( C     ,'         \\ \n" +
+"             `--'  .'           | \n" +
+"               |   |         .O | \n" +
+"             __|    \\        ,-'_ \n" +
+"            / `L     `._  _,'  ' `. \n" +
+"           /    `--.._  `',.   _\\  ` \n" +
+"           `-.       /\\  | `. ( ,\\  \\ \n" +
+"          _/  `-._  /  \\ |--'  (     \\ \n" +
+"         '  `-.   `'    \\/\\`.   `.    ) \n" +
+"               \\           \\ `.  |    | ");
+        
+        System.out.println("Hola, soy Pablo");
+    }
+
+}
